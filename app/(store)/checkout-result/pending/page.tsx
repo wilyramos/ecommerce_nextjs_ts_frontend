8219cf1373cff4d9728bc7fddbf6b726c55acce5
wilyramos <wily.ramos1@unmsm.edu.pe>
2025-07-05 14:42:12 -0500

    add page checkout results

---
+++ app/(store)/checkout-result/pending/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PendingPageCheckout() {
+    return (
+        <div>PendingPageCheckout</div>
+    )
+}
---
+++ app/(store)/checkout-result/success/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SuccessPageCheckout() {
+    return (
+        <>
+            <div>SuccessPageCheckout</div>
+            <p>Thank you for your purchase!</p>
+            <p>Your order has been successfully placed.</p>
+        </>
+    )
+}
