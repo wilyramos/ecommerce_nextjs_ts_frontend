8ea844fd7a6c07dd7406b16f934c53457c3b19a0
wilyramos <wily.ramos1@unmsm.edu.pe>
2025-07-07 18:27:09 -0500

    fix deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function NotFound() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center p-10">
                 <h1 className="text-4xl font-bold text-gray-800">404 - Página no encontrada</h1>
                 <p className="text-gray-500 mt-3 max-w-md">
                     Lo sentimos, no pudimos encontrar la página que estás buscando.
