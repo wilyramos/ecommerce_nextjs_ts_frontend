4950f2bc5aa7111aeaeb95155425ceccbb2b01f3
wilyramos <wily.ramos1@unmsm.edu.pe>
2026-05-08 12:14:34 -0500

    fix case-sensitive import

--- components/home/CarruselPrincipal.tsx
+++ components/home/CarruselPrincipal.tsx
@@ -1,3 +1,5 @@
+//File: frontend/components/home/Carruselprincipal.tsx
+
 import { SliderService } from "@/src/services/slider-service";
 import SliderBannerCarousel from '../banner/SliderBannerCarousel';
 
@@ -5,6 +7,7 @@ import SliderBannerCarousel from '../banner/SliderBannerCarousel';
 export default async function CarruselPrincipal() {
 
     const banners = await SliderService.getActive();
+    
     return (
         <SliderBannerCarousel banners={banners} />
     )
